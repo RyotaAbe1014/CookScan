e24bb3761a9e0c335273718a9299ab9dc3388013
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-11-23 15:08:18 +0900

    chore: add domain configuration for Vercel project
    - Introduced new resources for project domains in main.tf
    - Configured primary domain "cookscan.aberyouta.jp" and redirect domain "cook-scan.vercel.app" with a 307 redirect status

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,16 @@ resource "vercel_project" "cook-scan" {
     function_default_regions = ["hnd1"]
   }
 }
+
+resource "vercel_project_domain" "cook-scan" {
+  project_id = vercel_project.cook-scan.id
+  domain = "cookscan.aberyouta.jp"
+}
+
+resource "vercel_project_domain" "cook-scan-redirect" {
+  project_id = vercel_project.cook-scan.id
+  domain = "cook-scan.vercel.app"
+
+  redirect = vercel_project_domain.cook-scan.id
+  redirect_status_code = 307
+}
