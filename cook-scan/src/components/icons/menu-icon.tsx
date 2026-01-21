c1d7190f977e45c500d019f1810bfbea8718ea37
RyotaAbe1014 <bm1000bmmm@gmail.com>
2026-01-21 17:20:29 +0900

    feat: メニューアイコンの追加

---
+++ cook-scan/src/components/icons/menu-icon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from './types'
+
+export function MenuIcon({ className, ...props }: IconProps) {
+  return (
+    <svg
+      className={className}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  )
+}
