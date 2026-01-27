7caea26c58ca160b1cb20d1e3d6a51aff0bc2c3c
RyotaAbe1014 <bm1000bmmm@gmail.com>
2026-01-27 23:14:32 +0900

    🔧 chore: add typedRoutes

--- cook-scan/next.config.ts
+++ cook-scan/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@mastra/*"],
+  experimental: {
+    typedRoutes: true,
+  }
 };
 
 export default nextConfig;
