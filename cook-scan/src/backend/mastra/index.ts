23e3ddd0e3e99ce18d18e7112fa4db7180076a6b
RyotaAbe1014 <bm1000bmmm@gmail.com>
2026-02-23 02:16:28 +0900

    feat: MastraバックエンドのストレージにPostgresStoreを導入し、関連するMastraパッケージを更新

--- cook-scan/src/backend/mastra/index.ts
+++ cook-scan/src/backend/mastra/index.ts
@@ -1,6 +1,7 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
+import { PostgresStore } from "@mastra/pg";
 import { cookScanWorkflow } from './workflows/cook-scan-workflow';
 import { imageToTextAgent } from './agents/image-to-text-agent';
 import { convertTextToRecipeAgent } from './agents/convert-text-to-recipe-agent';
@@ -8,9 +9,15 @@ import { textToRecipeWorkflow } from './workflows/text-to-recipe';
 import { recommendAlternativesAgent } from './agents/recommend-alternative-agent';
 import { recommendAlternativesWorkflow } from './workflows/recommend-alternatives-workflow';
 
+const storage = new PostgresStore({
+  id: "pg-storage",
+  connectionString: process.env.DIRECT_URL,
+});
+
 export const mastra = new Mastra({
   workflows: { cookScanWorkflow, textToRecipeWorkflow, recommendAlternativesWorkflow },
   agents: { imageToTextAgent, convertTextToRecipeAgent, recommendAlternativesAgent },
+  storage: storage,
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
